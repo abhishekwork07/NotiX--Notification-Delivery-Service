3b6719e7ed8ece441aee6bca9f8503a8d931dff9
Abhishek Gupta <abhishek.work07@gmail.com>
2025-06-13 16:08:03 +0530

    Added connection check

--- api-service/src/main/java/com/abhishek/notix/api_service/ApiServiceApplication.java
+++ api-service/src/main/java/com/abhishek/notix/api_service/ApiServiceApplication.java
@@ -1,7 +1,12 @@
 package com.abhishek.notix.api_service;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ApiServiceApplication {
@@ -10,4 +15,21 @@ public class ApiServiceApplication {
 		SpringApplication.run(ApiServiceApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner kafkaCheck(KafkaTemplate<String, ?> kafkaTemplate) {
+		return args -> {
+			System.out.println("✅ KafkaTemplate is ready. Default topic config: " +
+					kafkaTemplate.getDefaultTopic());
+		};
+	}
+
+	@Bean
+	public ApplicationRunner dbTest(DataSource dataSource) {
+		return args -> {
+			System.out.println("✅ Connected to PostgreSQL at: " + dataSource.getConnection().getMetaData().getURL());
+		};
+	}
+
+
 }
+
