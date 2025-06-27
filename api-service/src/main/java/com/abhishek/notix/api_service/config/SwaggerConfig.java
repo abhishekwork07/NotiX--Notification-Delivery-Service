5d4e13501580c41c010f76d2561fe0c8bb4ee5ae
Abhishek Gupta <abhishek.work07@gmail.com>
2025-06-28 00:23:13 +0530

    Resolved Swagger Issue
    Learning - Always check for compatible versions once you start using the latest tech stack.

--- api-service/src/main/java/com/abhishek/notix/api_service/config/SwaggerConfig.java
+++ api-service/src/main/java/com/abhishek/notix/api_service/config/SwaggerConfig.java
@@ -1,7 +1,8 @@
 package com.abhishek.notix.api_service.config;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .openapi("3.0.n") // 🔑 Force OpenAPI 3.0.x
                 .info(new Info()
                         .title("NotiX Notification API")
                         .version("1.0")
-                        .description("API documentation for the NotiX notification delivery system"));
+                        .description("API documentation for the NotiX notification delivery system")
+                        .contact(new Contact().name("Abhishek Gupta").email("you@example.com"))
+                );
     }
 }
-
